521490f300401c4c10b1400af84d2c1bfcdeb487
Jeppe Dyrby <jeppe.dyrby@ebay.com>
2015-01-28 12:19:30 +0100

    simplify

--- ui-router-animate.js
+++ ui-router-animate.js
@@ -23,18 +23,12 @@ angular.module('uiRouterAnimate', ['ui.router'])
     }
 
   })
-  .directive('routerAnimate', function () {
+  .directive('routerAnimate', function ($rootScope, routerAnimate) {
     return {
       restrict: 'A',
       priority: 200,
-      controller: function ($scope, $rootScope, routerAnimate) {
-        var element;
-
+      link: function (scope, element, attrs) {
         var states = routerAnimate.getStates();
-
-        this.setElement = function (ele) {
-          element = ele;
-        };
 
         var getDirection = function (from, to) {
           var fromIndex = states.indexOf(from);
@@ -51,26 +45,15 @@ angular.module('uiRouterAnimate', ['ui.router'])
           return '';
         };
 
-        var unbind = $rootScope.$on('$stateChangeStart', function (e, to, _toParams, from, _fromParams) {
+        scope.$on('$stateChangeStart', function (e, to, _toParams, from, _fromParams) {
           if (!element) {
             return;
           }
           var direction = getDirection(from.name, to.name);
-          console.log(direction);
           element
             .removeClass('forward backward')
             .addClass(direction);
         });
-
-        $scope.$on('$destroy', function () {
-          console.log('destroy');
-          unbind();
-        })
-
-      },
-      link: function (scope, element, attrs, ctrl) {
-        ctrl.setElement(element);
-        console.log('create');
       }
     }
   });
